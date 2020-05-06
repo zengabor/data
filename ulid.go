4c07366a748a4a60f697c3a2dc2a6b6ff1c0a6e9
Gabor Lenard <zengabor@users.noreply.github.com>
2020-05-06 11:41:46 +0200

    Create ulid.go

---
+++ ulid.go
@@ -0,0 +1,22 @@
+package data
+
+import (
+	"math/rand"
+	// "strings"
+	"time"
+
+	"github.com/oklog/ulid"
+)
+
+func NewULID() string {
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+	u, err := ulid.New(ulid.Timestamp(time.Now().UTC()), entropy)
+	if err != nil {
+		return NewUUID4Str()
+	}
+	return string(u.String())
+}
+
+// func NewLowerCaseULID(t time.Time) string {
+// 	return strings.ToLower(NewULID(t))
+// }
